d69e4229bbd40bde79cb7985e0c8dac8413fca5a
SarahRector <rector.sarah.r@gmail.com>
2020-08-03 15:43:00 -0700

    added social class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const socialImages = [
+  'Email',
+  'RSS',
+  'Pinterest',
+  'Flickr',
+  'Instagram',
+  'GP',
+  'Twitter',
+  'Facebook'
+]
+
+class Social extends React.Component {
+  
+  render() {
+    return <p>
+      
+        {this.props.socialImage}
+      
+    </p>
+  }
 }
+
+class App extends React.Component {
+  render() {
+    return (
+      <header className="heading">
+        <div className="left">
+          <div>This is an image</div>
+          <div>
+            <h2>Delicious</h2>
+            <div>THE BEST FOOD BLOG ON THE WEB</div>
+          </div>
+        </div>
+        <div className="right">
+          {
+            socialImages.map(socialImage => (
+              <Social socialImage={socialImage} />
+
+            ))
+          }
+        </div>
+      </header>
+    );
+  }
+}
+    
 
 export default App;
